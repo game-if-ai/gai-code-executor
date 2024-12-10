7ad8f512e510fae8dbbbd004ec06e63a4a52f3c8
Aaron Shiel <ashiel@coldwasp.local>
2024-12-10 13:05:56 -0800

    clear tempdir

--- serverless/src/utils/executor.py
+++ serverless/src/utils/executor.py
@@ -4,7 +4,7 @@
 #
 # The full terms of this copyright and license should always be found in the root directory of this software deliverable as "license.txt" and if these terms are not found with this software, please contact the USC Stevens Center for the full license.
 #
-from typing import Tuple, Dict, Any
+from typing import Tuple
 from contextlib import redirect_stdout
 from io import StringIO
 from src.utils.bandit_manager import scan_user_code
@@ -20,13 +20,17 @@ log = get_logger("executor")
 
 
 @contextmanager
-def tempdir_cleanup_context(local_vars):
-    tempdir = local_vars.get("tempdir")
+def tempdir_cleanup_context():
+    namespace = {}
     try:
-        yield
+        yield namespace
     finally:
-        if tempdir and os.path.exists(tempdir):
+        log.info("cleaning up tempdir")
+        # Check for `tempdir` and clean up if it exists
+        tempdir = namespace.get("tempdir")
+        if tempdir and isinstance(tempdir, str) and os.path.isdir(tempdir):
             shutil.rmtree(tempdir)
+            log.info(f"Cleaned up tempdir: {tempdir}")
 
 
 def execute_code(code: str, uuid: str = "") -> Tuple[str, str, str]:
@@ -36,27 +40,26 @@ def execute_code(code: str, uuid: str = "") -> Tuple[str, str, str]:
         log.info("scanning code")
         (code_is_valid, bandit_result_as_string) = scan_user_code(code, uuid)
         if code_is_valid:
-            local_vars: Dict[str, Any] = {}
-            string_io = StringIO()
-            log.info("executing code")
-            with redirect_stdout(string_io):
-                with tempdir_cleanup_context(local_vars):
+            with tempdir_cleanup_context() as local_vars:
+                string_io = StringIO()
+                log.info("executing code")
+                with redirect_stdout(string_io):
                     exec(code, local_vars, local_vars)
-            console_output = string_io.getvalue()
-            log.info(console_output)
-            log.info("code executed")
-            if "result" in local_vars.keys():
-                log.info(local_vars["result"])
-                return (local_vars["result"], console_output, SUCCESS_STATE)
-            else:
-                local_vars.pop(
-                    "__builtins__"
-                )  # don't display the builtin variables.  It's a lot of information we don't need
-                return (
-                    f"no result variable instantiated.  Could not return result.  local vars:\n {str(local_vars)}",
-                    console_output,
-                    FAILURE_STATE,
-                )
+                console_output = string_io.getvalue()
+                log.info(console_output)
+                log.info("code executed")
+                if "result" in local_vars.keys():
+                    log.info(local_vars["result"])
+                    return (local_vars["result"], console_output, SUCCESS_STATE)
+                else:
+                    local_vars.pop(
+                        "__builtins__"
+                    )  # don't display the builtin variables.  It's a lot of information we don't need
+                    return (
+                        f"no result variable instantiated.  Could not return result.  local vars:\n {str(local_vars)}",
+                        console_output,
+                        FAILURE_STATE,
+                    )
         else:
             return (bandit_result_as_string, "", FAILURE_STATE)
     except Exception as e:
